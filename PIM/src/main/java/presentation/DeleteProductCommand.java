eede179709efd2e909b2356fed6f9f00d40b68f4
cprejler <prejler93@gmail.com>
2019-12-02 13:23:09 +0100

    changes to delete product

--- PIM/src/main/java/presentation/DeleteProductCommand.java
+++ PIM/src/main/java/presentation/DeleteProductCommand.java
@@ -11,6 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,25 +26,27 @@ public class DeleteProductCommand extends Command {
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) 
         throws ServletException, IOException, SQLException, ClassNotFoundException{
+        ProductMapper pMapper = new ProductMapper();
+        String webpage ="";
+        
         String productId = request.getParameter("ID");
         
-        String sqlStatement = "SELECT * from product WHERE productID='"+productId+"'";
-        Statement getColumnNames = connection.createStatement();
-        Statement statement = connection.createStatement();
-        ResultSet rs = getColumnNames.executeQuery(sqlStatement);
-        ResultSet result = statement.executeQuery(sqlStatement); 
+        Product product = pMapper.getProduct(Integer.parseInt(productId));
         
-        ArrayList fields = null;
+        try{
+            pMapper.deleteProduct(product);
+            
+        }catch(Exception e){
+            request.setAttribute("error", e.getMessage());
+            request.setAttribute("cause", e.getCause());
+            request.setAttribute("stacktrace", e.getStackTrace());
+            RequestDispatcher rd = request.getRequestDispatcher("Error.jsp");
+            rd.forward(request, response);
+            
+        }
         
         
-       // Product product = new Product(fields, name, category, type, manufacturer);
         
-        //Fra gamle prjekt, Vi burde måske lave en lignende metode
-        //Boolean usernameDB = db.checkUsername(username);
-        String webpage = "";
-        ProductMapper pM = new ProductMapper();
-        pM.deleteProduct(null);
-        webpage="Confirmation";
         return webpage;
         
         
