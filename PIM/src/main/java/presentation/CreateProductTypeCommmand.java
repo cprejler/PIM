7fc453f64962b4617ffeabdf04ae53640df8f5da
Jonathan Juhl <cph-jj450@cphbusiness.dk>
2019-12-03 15:50:42 +0100

    small changes

--- PIM/src/main/java/presentation/CreateProductTypeCommmand.java
+++ PIM/src/main/java/presentation/CreateProductTypeCommmand.java
@@ -113,9 +113,9 @@ public class CreateProductTypeCommmand extends Command {
 
         }
         
-        System.out.println("columns: " + generateproducts.toString());
-        System.out.println("columns: " + enumValues.get(0).toString());
-        System.out.println("columns: " + enumValues.get(1).toString());
+//        System.out.println("columns: " + generateproducts.toString());
+//        System.out.println("columns: " + enumValues.get(0).toString());
+//        System.out.println("columns: " + enumValues.get(1).toString());
         pMapper.createProductTable(generateproducts, enumValues);
         System.out.println("");
         return "ShowProducts";
